ead3cdb6146f3137987ace1a9155eaa73ba1ea4c
alexandrosok <alexandrosok@hotmail.com>
2019-09-25 12:30:35 +0100

    initializing

---
+++ src/pages/Root.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="">
+        <p>This is page 1 Root content</p>
+        <Form />
+    </div>
+
+</template>
+
+<script>
+
+    import Form from 'vue-form-component-v1-test'
+
+    export default {
+        name: "root",
+        components:{
+            Form
+        },
+        data() {
+            return {
+                device: null
+            }
+        },
+    }
+</script>
+
+<style>
+</style>
